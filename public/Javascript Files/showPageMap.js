c1fad6fea5d943a9ed9b054078e7124421cf2c91
Harshal <harshalubale4@gmail.com>
2022-01-28 16:07:27 +0530

    Centered the Map according to Coordinates

--- public/Javascript Files/showPageMap.js
+++ public/Javascript Files/showPageMap.js
@@ -2,6 +2,16 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    center: trekSpotInfo.info.geometry.coordinates, // starting position [lng, lat]
+    zoom: 13 // starting zoom
 });
+
+const markerOfCity = new mapboxgl.Marker({ color: 'black', rotation: 45 })
+    .setLngLat(trekSpotInfo.info.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3> ${trekSpotInfo.info.title}</h3><p> ${trekSpotInfo.info.location}</p>`
+            )
+    )
+    .addTo(map);
