7dbb44d498f4781cc52c3d2377deeadcaffdc16a
Harshal <harshalubale4@gmail.com>
2022-01-31 14:57:31 +0530

    Trying to Deploy

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const reviewRoutes = require("./routes/reviews")
 const userRoutes = require("./routes/users");
 const flash = require('connect-flash');
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/trekker'
-const secret = process.env.SECRET || 'thisisagoofysecret';
+const secret = 'thisisagoofysecret';
 
 mongoose.connect(dbUrl)
     .then(() => {
